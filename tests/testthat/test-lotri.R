36c38fd8ec25fa02668e03000ddcc821ff85e446
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2021-02-07 10:07:23 -0600

    Add another test case for lotri error

--- tests/testthat/test-lotri.R
+++ tests/testthat/test-lotri.R
@@ -188,7 +188,8 @@ test_that("lotri matrix parsing", {
   )
 
   ## Parsing errors
-  expect_error(lotri(~ c(40)))
+  expect_error(lotri(~ c(40))) #nolint
+  expect_error(lotri(~ 40))
   expect_error(lotri(a ~ c(3, 1, 3)))
   expect_error(lotri(a ~ c(3, 1)))
   expect_error(lotri({
