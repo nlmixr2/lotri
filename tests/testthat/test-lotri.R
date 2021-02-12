62a6263639edc0c745a358da6f540538fa1e6414
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2021-02-11 20:48:01 -0600

    Add comparison outputs

--- tests/testthat/test-lotri.R
+++ tests/testthat/test-lotri.R
@@ -1631,11 +1631,17 @@ test_that("lotri matrix parsing", {
                lotri(c + d ~ c(0.1,
                                0.001, 0.1)))
 
+  expect_equal(attr(tmp, "lotriFix"),
+               structure(c(TRUE, TRUE, FALSE, FALSE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE), .Dim = c(4L, 4L), .Dimnames = list(c("a", "b", "c", "d"), c("a", "b", "c", "d"))))
+
   tmp <- lotri(a + b + c ~ c(
     fix(40),
     0.1, 20,
     0.1, 0.1, 30
   ))
+
+  expect_equal(attr(tmp, "lotriFix"),
+               structure(c(TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE), .Dim = c(3L, 3L), .Dimnames = list(c("a", "b", "c"),     c("a", "b", "c"))))
 
   tmp <- lotri(a + b + c ~ c(
     40,
@@ -1643,10 +1649,16 @@ test_that("lotri matrix parsing", {
     0.1, 0.1, 30
   ))
 
+  expect_equal(attr(tmp, "lotriFix"),
+               structure(c(FALSE, TRUE, FALSE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE), .Dim = c(3L, 3L), .Dimnames = list(c("a", "b", "c"),     c("a", "b", "c"))) )
+
   tmp <- lotri(a + b + c ~ c(
     40,
     0.1, 20,
     fix(0.1), 0.1, 30
   ))
+
+  expect_equal(attr(tmp, "lotriFix"),
+               structure(c(FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE), .Dim = c(3L, 3L), .Dimnames = list(c("a", "b", "c"),     c("a", "b", "c"))))
 
   })
