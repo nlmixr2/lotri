f2ce1a77977c8707b5bd54117a7021c3372836b4
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2021-02-13 00:06:35 -0600

    Updates for bad conditions

--- R/thetaEst.R
+++ R/thetaEst.R
@@ -24,14 +24,18 @@
 .parseThetaEstFix <- function(x, envir=parent.frame()) {
   .env <- new.env(parent=emptyenv())
   .env$fix <- FALSE
-  x <- .parseThetaEstFixQ(x, .env)
-  .ret <- as.numeric(eval(x, envir=envir))
-  if (length(.ret) == 1) {
+  .x <- .parseThetaEstFixQ(x, .env)
+  .ret <- try(eval(.x, envir=envir), silent=TRUE)
+  if (inherits(.ret, "try-error")) return(deparse1(x))
+  .numeric <- vapply(.ret, is.numeric, logical(1))
+  if (!all(.numeric)) {
+    return(deparse1(x))
+  } else if (length(.ret) == 1) {
     .ret <- c(-Inf, .ret, Inf)
   } else if (length(.ret) == 2) {
     .ret <- c(.ret, Inf)
   } else if (length(.ret) != 3) {
-    stop("estimate syntax unsupported: ", deparse(x), call.=FALSE)
+    return(deparse1(x))
   }
   data.frame(lower=.ret[1], est=.ret[2], upper=.ret[3], fix=.env$fix)
 }
@@ -45,8 +49,13 @@
        lapply(x[[2]], .parseThetaEstQ, env=env, envir = envir)
      } else if  (identical(x[[1]], quote(`<-`)) ||
                    identical(x[[1]], quote(`=`))) {
-
-       env$df <- c(env$df, list(data.frame(name=as.character(x[[2]]), .parseThetaEstFix(x[[3]], envir=envir))))
+       .name <- as.character(x[[2]])
+       .df <- .parseThetaEstFix(x[[3]], envir=envir)
+       if (inherits(.df, "data.frame")) {
+         env$df <- c(env$df, list(data.frame(name=.name, .df)))
+       } else {
+         env$err <- c(env$err, paste0("estimate syntax unsupported: ", .name, " ", deparse(x[[1]]), " ", .df))
+       }
      } else {
       return(as.call(lapply(x, .parseThetaEstQ, env=env, envir=envir)))
     }
@@ -58,9 +67,11 @@
 .parseThetaEst <- function(x, envir=parent.frame()) {
   .env <- new.env(parent=emptyenv())
   .env$df <- NULL
+  .env$err <- NULL
   .parseThetaEstQ(x, .env, envir=envir)
   if (!is.null(.env$df)){
     .env$df <- do.call(rbind, .env$df)
   }
+  print(.env$err)
   return(.env$df)
 }
