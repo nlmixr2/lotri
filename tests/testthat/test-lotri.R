68aee42557cc8118a1c874f3216ca2dd2bd30818
Matthew Fidler <matthew.fidler@gmail.com>
2021-02-17 18:50:11 -0600

    Start thetaEst tests

--- tests/testthat/test-lotri.R
+++ tests/testthat/test-lotri.R
@@ -1674,6 +1674,54 @@ test_that("lotri matrix parsing", {
                structure(c(FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE), .Dim = c(3L, 3L), .Dimnames = list(c("a", "b", "c"),     c("a", "b", "c"))))
 
   expect_error(capture.output(print(tmp)), NA)
+  
+  context("Combined estimates and matrix")
+  
+  fix1 <- lotri({
+    a <- c(0, 1); backTransform("exp"); label("a label")
+    b <- c(0, 1, 2) 
+    c <- fix(1)
+    d <- fix(0, 1, 2)
+    e <- c(0, 1, 2, fixed)
+    f+g ~ c(1,
+            0.5, 1)
+  })
+  
+  expect_equal(attr(fix1, "lotriEst"),
+               structure(list(name = c("a", "b", "c", "d", "e"), 
+                              lower = c(0, 0, -Inf, 0, 0), 
+                              est = c(1, 1, 1, 1, 1), 
+                              upper = c(Inf, 2, Inf, 2, 2), 
+                              fix = c(FALSE, FALSE, TRUE, TRUE, TRUE), 
+                              label = c("a label",NA, NA, NA, NA), 
+                              backTransform = c("exp", NA, NA, NA, NA)), 
+                         row.names = c(NA,-5L), class = "data.frame"))
+  fix2 <- fix1
+  attr(fix2, "lotriEst") <- NULL
+  class(fix2) <- NULL
+  
+  expect_equal(fix2,
+               structure(c(1, 0.5, 0.5, 1), 
+                         .Dim = c(2L, 2L), 
+                         .Dimnames = list(c("f", "g"), c("f", "g"))))
+  
+  fix2 <- lotri({
+    a = c(3); label(matt); backTransform(exp)
+  })
+  
+  expect_equal(attr(fix2, "lotriEst"),
+               structure(list(name = "a", 
+                              lower = -Inf, 
+                              est = 3, 
+                              upper = Inf, 
+                              fix = FALSE, 
+                              label = "matt", 
+                              backTransform = "exp"), 
+                         row.names = c(NA, -1L), 
+                         class = "data.frame"))
+  
+  expect_error(lotri({a = "matt"}))
+  
 
 })
 
