e87662c21ae92cb26500f69fc7a2cfae8f11bb5c
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2021-02-24 14:36:12 -0600

    Remove library(hexSticker) from lint; not used in package directly

--- inst/lotriHex.R
+++ inst/lotriHex.R
@@ -1,4 +1,4 @@
-library(hexSticker)
+library(hexSticker) # nolint
 sticker("lotri.svg", package="lotri", p_size=20, s_x=1, s_y=.85, s_width=.55,
         h_fill = "#dfdfdf",
         h_color = "#4d4d4d",
