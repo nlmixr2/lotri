ea47445fb9f6948f06ebfd8c801ecc4bf830beb7
Bill Denney <wdenney@humanpredictions.com>
2022-01-16 11:01:32 -0500

    Convert multiple messages into a single message

--- R/err.R
+++ R/err.R
@@ -6,17 +6,25 @@
 ##' @noRd
 .printErr <- function(.env) {
   if (.env$.hasErr) {
-    message("\033[1mlotri syntax error:\n================================================================================\033[0m")
+    messageText <-
+      "\033[1mlotri syntax error:\n================================================================================\033[0m"
     for (i in seq_along(.env$.err)) {
       if (!is.null(.env$.err[[i]])) {
-        message(crayon::bold("lotri error:"))
-        .err <- paste(paste("  ", strsplit(.env$.err[[i]], "\n")[[1]]), collapse="\n")
-        message(.err)
+        messageText <-
+          c(
+            messageText,
+            crayon::bold("lotri error:"),
+            paste(paste("  ", strsplit(.env$.err[[i]], "\n")[[1]]), collapse="\n")
+          )
       }
-      message(.env$.lines[i])
+      messageText <- c(messageText, .env$.lines[i])
     }
-    message("\033[1m================================================================================\033[0m")
-    ## message(paste(.env$.lines, collapse="\n"))
+    messageText <-
+      c(
+        messageText,
+        "\033[1m================================================================================\033[0m"
+      )
+    message(paste(messageText, collapse="\n"))
     stop("lotri syntax errors above", call. = FALSE)
   }
 }
